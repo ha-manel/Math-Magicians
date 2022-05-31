daf17ffb93da2a3e1e139f37e7e20f56014a9fb5
Manel <manelhammouche3@gmail.com>
2022-05-31 12:45:04 +0100

    Update routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
