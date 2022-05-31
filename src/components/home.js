c77040a3cdf280eca263e26da284e88ca775ce2c
Manel <manelhammouche3@gmail.com>
2022-05-31 11:25:50 +0100

    Add home component

---
+++ src/components/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => (
+  <div id="home-page">
+    <h2 id="home-header">Welcome to our page</h2>
+    <p id="home-text">
+      Mathematics is the science that deals with the logic of shape, quantity and arrangement.
+      Math is all around us, in everything we do.
+      It is the building block for everything in our daily lives,
+      including mobile devices, computers, software, architecture (ancient and modern),
+      art, money, engineering and even sports.
+    </p>
+  </div>
+);
+
+export default Home;
