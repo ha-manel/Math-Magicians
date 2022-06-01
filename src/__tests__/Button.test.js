0390ab56a8c165a94414ed7f00c6ac0ef1344f0e
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-01 16:09:50 +0200

    create test for buttons

---
+++ src/__tests__/Button.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render, fireEvent, screen } from '@testing-library/react';
+import Button from '../components/button';
+import '@testing-library/jest-dom';
+ 
+describe('Testing the button component', () => {
+  const calculateResult = jest.fn();
+ 
+  it('Button component is rendered', () => {
+    const button = renderer
+      .create(<Button eventHandler={calculateResult} value="AC" />)
+      .toJSON();
+    expect(button).toMatchSnapshot();
+  });
+ 
+  it('Test button click', () => {
+    render(<Button eventHandler={calculateResult} value="AC" />);
+    fireEvent.click(screen.getByText('AC'));
+    fireEvent.click(screen.getByText('AC'));
+    fireEvent.click(screen.getByText('AC'));
+    expect(calculateResult).toHaveBeenCalledTimes(3);
+  });
+});
