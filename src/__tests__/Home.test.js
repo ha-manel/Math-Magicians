f6b10fcdf2a6953a668830aabfec356b6220e409
Manel <manelhammouche3@gmail.com>
2022-06-01 15:33:57 +0100

    Fix operate test bug

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Home from '../components/home';
 import '@testing-library/jest-dom';
- 
+
 describe('Test of home page', () => {
   it('test if home page is rendered', () => {
     const homePage = renderer
--- src/__tests__/operate.test.js
+++ src/__tests__/operate.test.js
@@ -1,16 +1,16 @@
 import operate from '../logic/operate';
 
 describe('testing the operate function', () => {
-    test('addition test', () => {
-        expect(operate(10, 29, '+')).toBe(39);
-    });
-    test('division test', () => {
-        expect(operate(10, 2, '÷')).toBe(5);
-    });
-    test('multiplication test', () => {
-        expect(operate(10, 29, 'x')).toBe(290);
-    });
-    test('modulo test', () => {
-        expect(operate(9, 4, '%')).toBe(1);
-    });
+  test('addition test', () => {
+    expect(operate(10, 29, '+')).toEqual('39');
+  });
+  test('division test', () => {
+    expect(operate(10, 2, '÷')).toEqual('5');
+  });
+  test('multiplication test', () => {
+    expect(operate(10, 29, 'x')).toEqual('290');
+  });
+  test('modulo test', () => {
+    expect(operate(9, 4, '%')).toEqual('1');
+  });
 });
